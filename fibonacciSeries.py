39f64aa91d644d4490270c822f9ebb4bdeaed64e
Dit <145139482+Elite011@users.noreply.github.com>
2024-05-17 23:05:22 +0530

    Day one

---
+++ fibonacciSeries.py
@@ -0,0 +1,28 @@
+'''Fibonacci series for the Number user will input'''
+
+#If you want to find the fibonacci numbers under twenty.
+
+# def fibonacci(n):
+#     a,b=0,1
+#     while a<=n:
+#         print(a,end=' ') #using "end=' '" to make space between the numbers
+#         a,b=b,a+b
+        
+# n=int(input("Enter you number:\n"))
+# fibonacci(n)
+
+#Another way if you want the first twenty numbers of fibonacci series in an loop.
+
+def fibonacci(n):
+    a,b=0,1
+    for i in range (n):
+        print(a,end=" ")
+        a,b=b,a+b
+    
+while True:
+
+    n=int(input("\nEnter a number:"))
+    fibonacci(n)
+    if n==0:
+        break
+
