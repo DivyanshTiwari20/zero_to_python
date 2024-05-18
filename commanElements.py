8ce65b4578225266a051c7079af66ab21ac28ec7
Dit <145139482+Elite011@users.noreply.github.com>
2024-05-18 10:53:49 +0530

    Day two

---
+++ commanElements.py
@@ -0,0 +1,10 @@
+'''Write a function that takes two lists as its arguments and returns a new list that contains all the common elements of the two input lists.'''
+
+listOne={"apple", "mango"}
+listTwo={"siri","mango" }
+
+listNew=listOne.union(listTwo)
+
+print(listNew)
+
+
