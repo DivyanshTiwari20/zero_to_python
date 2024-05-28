99bf61f36fc544b79975c852bb5b14fb717c2c8f
Dit <145139482+Elite011@users.noreply.github.com>
2024-05-28 11:52:37 +0530

    28/5/2024-Afternoon-11:52AM

--- leetCodeProblems/palindromeNum.py
+++ leetCodeProblems/palindromeNum.py
@@ -3,7 +3,12 @@ Given an integer x, return true if x is a
 palindrome, and false otherwise.
 '''
 
+class Solution:
+    def isPalindrome(self,x:int)-> bool:
+        converStrinInt=str(x)
 
-class sol():
-    def isPalindrome():
-        pass
+        return converStrinInt==converStrinInt[::-1]
+
+#Example usage:
+solution=Solution()
+print(solution.isPalindrome(828))
---
+++ leetCodeProblems/romanToInt.py
@@ -0,0 +1,30 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        # Define a dictionary with Roman numeral values
+        romantoInt = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        prev_value = 0
+        
+        # Iterate over the Roman numeral string from end to start
+        for char in reversed(s):
+            value = romantoInt[char]
+            if value < prev_value:
+                total -= value
+            else:
+                total += value
+            prev_value = value
+        
+        return total
+
+# Example usage:
+solution = Solution()
+print(solution.romanToInt("MCMXCIV"))  # Output: 1994
