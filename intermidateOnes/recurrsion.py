c7f7e5b8e2506fc5dedbbfd24c187b594632749d
Dit <145139482+Elite011@users.noreply.github.com>
2024-05-29 19:22:10 +0530

    no comment

--- intermidateOnes/recurrsion.py
+++ intermidateOnes/recurrsion.py
@@ -1,1 +1,13 @@
-'''Solving the Tower of Hanoi problem is a classic recursive problem that helps understand the principles of recursion.'''
+'''Solving the Tower of Hanoi problem is a classic recursive problem
+that helps understand the principles of recursion.'''
+
+def towerHanoi(n,a,b,c):
+	if n==1:
+		print("move 1st disk from ",a, " to ", c)
+		return
+	towerHanoi(n-1,a,c,b)
+	print("move",n,"the disk from",a,"to",c)
+	towerHanoi(n-1,b,a,c)
+
+towerHanoi(14,"s","h","d")
+
