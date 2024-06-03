1002ec12f3f099e9937ac81dcd0200aee61c7654
Dit <145139482+Elite011@users.noreply.github.com>
2024-06-03 10:17:59 +0530

    Easy

--- leetCodeProblems/longestCommonPrefix.py
+++ leetCodeProblems/longestCommonPrefix.py
@@ -2,4 +2,23 @@
 Write a function to find the longest common prefix string amongst an array of strings.
 
 If there is no common prefix, return an empty string
+
 '''
+class Solution:
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+        
+        pref = strs[0]
+        pref_len = len(pref)
+
+        for s in strs[1:]:
+            while pref != s[0:pref_len]:
+                pref_len -= 1
+                if pref_len == 0:
+                    return ""
+                pref = pref[0:pref_len]
+
+        return pref
+
+    
---
+++ leetCodeProblems/validParentheses.py
@@ -0,0 +1,26 @@
+'''
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+'''
+
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+
+        return not stack
+
+
